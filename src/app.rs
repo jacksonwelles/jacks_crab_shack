12ae07c6672712058c66ce6c34933cbf945233fa
Jackson Welles <jkwelles@gmail.com>
2025-03-14 21:10:33 +0000

    progress bar example

--- src/app.rs
+++ src/app.rs
@@ -3,15 +3,29 @@ use leptos::prelude::*;
 #[component]
 pub fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
+    let double_count = move || count.get() * 2;
 
-    view! {
+    return view! {
         <button
-            on:click=move |_| *set_count.write() += 1
-            class:red=move || count.get() % 2 == 1
+            on:click=move |_| {*set_count.write() += 1; }
         >
-            "Click me: "
-            {count}
+            "Click to Move" {count}
         </button>
-        <p>"Double count: " {move || count.get() * 2}</p>
+        <ProgressBar progress= move || count.get()/>
+        <ProgressBar progress=double_count/>
+    }
+}
+
+#[component]
+fn ProgressBar(
+    #[prop(default = 100)]
+    max: u16,
+    progress: impl Fn() -> i32 + Send + Sync + 'static
+) -> impl IntoView {
+    view! {
+        <progress
+            max=max
+            value=progress
+        />
     }
 }
