f47bb47ad659416d8645ae6c6ed3ab6fd078b4a2
Jackson Welles <jkwelles@gmail.com>
2025-03-17 21:34:04 +0000

    compiling webgl

--- src/app.rs
+++ src/app.rs
@@ -1,56 +1,154 @@
+use leptos::prelude::*;
+use leptos::html::Canvas;
 use leptos::wasm_bindgen::prelude::*;
-use leptos::{html::Canvas, prelude::*};
-use std::f64;
+
+use web_sys::{WebGlShader, WebGlProgram, WebGl2RenderingContext};
+use web_sys::js_sys;
 
 #[component]
 pub fn App() -> impl IntoView {
     let canvas_ref = NodeRef::<Canvas>::new();
-    let (count, set_count) = signal(0);
+    
+    let context = canvas_ref
+        .get()
+        .expect("get_untracked")
+        .get_context("webgl2")
+        .expect("get_context")
+        .expect("object")
+        .dyn_into::<WebGl2RenderingContext>().unwrap();
 
-    Effect::new(move |_| {
-        if let Some(canvas) = canvas_ref.get() {
-            let context = canvas
-                .get_context("2d")
-                .unwrap()
-                .unwrap()
-                .dyn_into::<web_sys::CanvasRenderingContext2d>()
-                .unwrap();
-            context.reset();
-            context.begin_path();
+    let vert_shader = compile_shader(
+        &context,
+        WebGl2RenderingContext::VERTEX_SHADER,
+        r##"#version 300 es
+ 
+        in vec4 position;
 
-            // Draw the outer circle.
-            context
-                .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-                .unwrap();
-            
-            // Draw the mouth.
-            if count.get() % 2 == 0 {    
-                context.move_to(100.0, 80.0);
-                context.arc(75.0, 80.0, 25.0, 0.0,  f64::consts::PI).unwrap();
-            } else {
-                context.move_to(50.0, 105.0);
-                context.arc(75.0, 105.0, 25.0, f64::consts::PI,  0.0).unwrap();
-            }
-
-
-            // Draw the left eye.
-            context.move_to(65.0, 65.0);
-            context
-                .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-                .unwrap();
-
-            // Draw the right eye.
-            context.move_to(95.0, 65.0);
-            context
-                .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-                .unwrap();
-
-            context.stroke();
+        void main() {
+        
+            gl_Position = position;
         }
-    });
+        "##,
+    ).unwrap();
+
+    let frag_shader = compile_shader(
+        &context,
+        WebGl2RenderingContext::FRAGMENT_SHADER,
+        r##"#version 300 es
+    
+        precision highp float;
+        out vec4 outColor;
+        
+        void main() {
+            outColor = vec4(1, 1, 1, 1);
+        }
+        "##,
+    ).unwrap();
+    let program = link_program(&context, &vert_shader, &frag_shader).unwrap();
+    context.use_program(Some(&program));
+
+    let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
+
+    let position_attribute_location = context.get_attrib_location(&program, "position");
+    let buffer = context.create_buffer().ok_or("Failed to create buffer").unwrap();
+    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
+
+    // Note that `Float32Array::view` is somewhat dangerous (hence the
+    // `unsafe`!). This is creating a raw view into our module's
+    // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
+    // (aka do a memory allocation in Rust) it'll cause the buffer to change,
+    // causing the `Float32Array` to be invalid.
+    //
+    // As a result, after `Float32Array::view` we have to be very careful not to
+    // do any memory allocations before it's dropped.
+    unsafe {
+        let positions_array_buf_view = js_sys::Float32Array::view(&vertices);
+
+        context.buffer_data_with_array_buffer_view(
+            WebGl2RenderingContext::ARRAY_BUFFER,
+            &positions_array_buf_view,
+            WebGl2RenderingContext::STATIC_DRAW,
+        );
+    }
+
+    let vao = context
+        .create_vertex_array()
+        .ok_or("Could not create vertex array object").unwrap();
+    context.bind_vertex_array(Some(&vao));
+
+    context.vertex_attrib_pointer_with_i32(
+        position_attribute_location as u32,
+        3,
+        WebGl2RenderingContext::FLOAT,
+        false,
+        0,
+        0,
+    );
+    context.enable_vertex_attrib_array(position_attribute_location as u32);
+
+    context.bind_vertex_array(Some(&vao));
+
+    let vert_count = (vertices.len() / 3) as i32;
+    draw(&context, vert_count);
 
     view! {
-      <button on:click=move |_| {*set_count.write() += 1;}> "Flip" </button>
       <canvas node_ref=canvas_ref/>
+    }
+}
+
+fn draw(context: &WebGl2RenderingContext, vert_count: i32) {
+    context.clear_color(0.0, 0.0, 0.0, 1.0);
+    context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+
+    context.draw_arrays(WebGl2RenderingContext::TRIANGLES, 0, vert_count);
+}
+
+pub fn compile_shader(
+    context: &WebGl2RenderingContext,
+    shader_type: u32,
+    source: &str,
+) -> Result<WebGlShader, String> {
+    let shader = context
+        .create_shader(shader_type)
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+    context.shader_source(&shader, source);
+    context.compile_shader(&shader);
+
+    if context
+        .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(shader)
+    } else {
+        Err(context
+            .get_shader_info_log(&shader)
+            .unwrap_or_else(|| String::from("Unknown error creating shader")))
+    }
+}
+
+pub fn link_program(
+    context: &WebGl2RenderingContext,
+    vert_shader: &WebGlShader,
+    frag_shader: &WebGlShader,
+) -> Result<WebGlProgram, String> {
+    let program = context
+        .create_program()
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+
+    context.attach_shader(&program, vert_shader);
+    context.attach_shader(&program, frag_shader);
+    context.link_program(&program);
+
+    if context
+        .get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(program)
+    } else {
+        Err(context
+            .get_program_info_log(&program)
+            .unwrap_or_else(|| String::from("Unknown error creating program object")))
     }
 }
