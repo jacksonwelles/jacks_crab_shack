57e65cf9f2dec526d308e1e516394cd96c60cc40
Jackson Welles <jkwelles@gmail.com>
2025-03-28 19:38:52 +0000

    resizeable figure

--- src/app.rs
+++ src/app.rs
@@ -1,98 +1,115 @@
-use leptos::prelude::*;
 use leptos::html::Canvas;
+use leptos::prelude::*;
 use leptos::wasm_bindgen::prelude::*;
 
-use web_sys::{WebGlShader, WebGlProgram, WebGl2RenderingContext};
 use web_sys::js_sys;
+use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlShader};
+use web_sys::console;
 
 #[component]
 pub fn App() -> impl IntoView {
     let canvas_ref = NodeRef::<Canvas>::new();
-    
-    let context = canvas_ref
-        .get()
-        .expect("get_untracked")
-        .get_context("webgl2")
-        .expect("get_context")
-        .expect("object")
-        .dyn_into::<WebGl2RenderingContext>().unwrap();
+    let (count, set_count) = signal(0);
+    Effect::new(move |_| {
+        if let Some(canvas) = canvas_ref.get() {
+            let context = canvas
+                .get_context("webgl2")
+                .expect("get_context")
+                .expect("object")
+                .dyn_into::<WebGl2RenderingContext>()
+                .unwrap();
 
-    let vert_shader = compile_shader(
-        &context,
-        WebGl2RenderingContext::VERTEX_SHADER,
-        r##"#version 300 es
+            let vert_shader = compile_shader(
+                &context,
+                WebGl2RenderingContext::VERTEX_SHADER,
+                r##"#version 300 es
  
-        in vec4 position;
+                in vec4 position;
 
-        void main() {
-        
-            gl_Position = position;
-        }
-        "##,
-    ).unwrap();
+                void main() {
+                
+                    gl_Position = position;
+                }
+                "##,
+            )
+            .unwrap();
 
-    let frag_shader = compile_shader(
-        &context,
-        WebGl2RenderingContext::FRAGMENT_SHADER,
-        r##"#version 300 es
+            let frag_shader = compile_shader(
+                &context,
+                WebGl2RenderingContext::FRAGMENT_SHADER,
+                r##"#version 300 es
     
-        precision highp float;
-        out vec4 outColor;
-        
-        void main() {
-            outColor = vec4(1, 1, 1, 1);
+                precision highp float;
+                out vec4 outColor;
+                
+                void main() {
+                    outColor = vec4(1, 1, 1, 1);
+                }
+                "##,
+            )
+            .unwrap();
+            let program = link_program(&context, &vert_shader, &frag_shader).unwrap();
+            context.use_program(Some(&program));
+
+            let factor: f32 = count.get() as f32 / 50.0;
+            let mut vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
+            vertices.iter_mut().for_each(|point|{*point = *point * factor;});
+            let vertices = vertices;
+
+            let position_attribute_location = context.get_attrib_location(&program, "position");
+            let buffer = context
+                .create_buffer()
+                .ok_or("Failed to create buffer")
+                .unwrap();
+            context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
+
+            // Note that `Float32Array::view` is somewhat dangerous (hence the
+            // `unsafe`!). This is creating a raw view into our module's
+            // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
+            // (aka do a memory allocation in Rust) it'll cause the buffer to change,
+            // causing the `Float32Array` to be invalid.
+            //
+            // As a result, after `Float32Array::view` we have to be very careful not to
+            // do any memory allocations before it's dropped.
+            unsafe {
+                let positions_array_buf_view = js_sys::Float32Array::view(&vertices);
+
+                context.buffer_data_with_array_buffer_view(
+                    WebGl2RenderingContext::ARRAY_BUFFER,
+                    &positions_array_buf_view,
+                    WebGl2RenderingContext::STATIC_DRAW,
+                );
+            }
+
+            let vao = context
+                .create_vertex_array()
+                .ok_or("Could not create vertex array object")
+                .unwrap();
+            context.bind_vertex_array(Some(&vao));
+
+            context.vertex_attrib_pointer_with_i32(
+                position_attribute_location as u32,
+                3,
+                WebGl2RenderingContext::FLOAT,
+                false,
+                0,
+                0,
+            );
+            context.enable_vertex_attrib_array(position_attribute_location as u32);
+
+            context.bind_vertex_array(Some(&vao));
+
+            let vert_count = (vertices.len() / 3) as i32;
+            draw(&context, vert_count);
         }
-        "##,
-    ).unwrap();
-    let program = link_program(&context, &vert_shader, &frag_shader).unwrap();
-    context.use_program(Some(&program));
-
-    let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
-
-    let position_attribute_location = context.get_attrib_location(&program, "position");
-    let buffer = context.create_buffer().ok_or("Failed to create buffer").unwrap();
-    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
-
-    // Note that `Float32Array::view` is somewhat dangerous (hence the
-    // `unsafe`!). This is creating a raw view into our module's
-    // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
-    // (aka do a memory allocation in Rust) it'll cause the buffer to change,
-    // causing the `Float32Array` to be invalid.
-    //
-    // As a result, after `Float32Array::view` we have to be very careful not to
-    // do any memory allocations before it's dropped.
-    unsafe {
-        let positions_array_buf_view = js_sys::Float32Array::view(&vertices);
-
-        context.buffer_data_with_array_buffer_view(
-            WebGl2RenderingContext::ARRAY_BUFFER,
-            &positions_array_buf_view,
-            WebGl2RenderingContext::STATIC_DRAW,
-        );
-    }
-
-    let vao = context
-        .create_vertex_array()
-        .ok_or("Could not create vertex array object").unwrap();
-    context.bind_vertex_array(Some(&vao));
-
-    context.vertex_attrib_pointer_with_i32(
-        position_attribute_location as u32,
-        3,
-        WebGl2RenderingContext::FLOAT,
-        false,
-        0,
-        0,
-    );
-    context.enable_vertex_attrib_array(position_attribute_location as u32);
-
-    context.bind_vertex_array(Some(&vao));
-
-    let vert_count = (vertices.len() / 3) as i32;
-    draw(&context, vert_count);
+        console::log_1(&"LOOOOOG".into());
+    });
 
     view! {
-      <canvas node_ref=canvas_ref/>
+        <button on:click=move |_| {
+            *set_count.write() += 1;
+        }>"Click me: " {count}</button>
+        <canvas node_ref=canvas_ref />
     }
 }
 
