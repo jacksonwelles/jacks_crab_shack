f6a58d368c7eb56786f6f56ea5e88702f0c64649
Jackson Welles <jkwelles@gmail.com>
2025-07-19 03:19:48 +0000

    use new macro for fluids

--- src/fluids.rs
+++ src/fluids.rs
@@ -1,4 +1,4 @@
-use super::common::*;
+use utility::prelude::*;
 
 use std::cell::RefCell;
 use std::cmp;
@@ -83,380 +83,17 @@ pub fn App() -> impl IntoView {
     }
 }
 
-// TODO: These pipeline structs are super generic.
-// There must be a way to make them with macros instead of by hand...
-struct BoundaryPipeline {
-    program: Program,
-    scale: f32,
-    texel_size: (f32, f32),
-}
+render_pipeline!(AdvectPipeline, "shaders/advect.frag");
 
-impl BoundaryPipeline {
-    const TARGET: i32 = 0;
-    const OFFSET: i32 = 1;
-    pub fn create(context: &WebGl2RenderingContext, program: Program) -> BoundaryPipeline {
-        context.use_program(Some(program.program()));
-        context.uniform1i(
-            program
-                .uniforms()
-                .get("u_target")
-                .expect("missing argument")
-                .into(),
-            Self::TARGET,
-        );
-        context.uniform1i(
-            program
-                .uniforms()
-                .get("u_boundary_offsets")
-                .expect("missing argument")
-                .into(),
-            Self::OFFSET,
-        );
-        BoundaryPipeline {
-            program,
-            scale: 0.0,
-            texel_size: (0.0, 0.0),
-        }
-    }
-    pub fn set_arguments(
-        &mut self,
-        context: &WebGl2RenderingContext,
-        scale: f32,
-        boundary_texture: &BufferedTexture,
-        target_texture: &BufferedTexture,
-    ) -> () {
-        debug_assert!(boundary_texture.texel_size() == target_texture.texel_size());
-        context.use_program(Some(self.program.program()));
-        target_texture.attach(Self::TARGET);
-        boundary_texture.attach(Self::OFFSET);
+render_pipeline!(ImpulsePipeline, "shaders/force.frag");
 
-        if self.scale != scale {
-            context.uniform1f(
-                self.program
-                    .uniforms()
-                    .get("u_scale")
-                    .expect("missing argument")
-                    .into(),
-                scale,
-            );
-            self.scale = scale;
-        }
-        if self.texel_size != target_texture.texel_size() {
-            context.uniform2f(
-                self.program
-                    .uniforms()
-                    .get("u_texel_size")
-                    .expect("missing argument")
-                    .into(),
-                target_texture.texel_size().0,
-                target_texture.texel_size().1,
-            );
-            self.texel_size = target_texture.texel_size();
-        }
-    }
-}
+render_pipeline!(DivergencePipeline, "shaders/divergence.frag");
 
-struct AdvectPipeline {
-    program: Program,
-    timestep: f32,
-    target_texel_size: (f32, f32),
-    velocity_texel_size: (f32, f32),
-}
+render_pipeline!(JacobiPipeline, "shaders/jacobi.frag");
 
-impl AdvectPipeline {
-    const TARGET: i32 = 0;
-    const VELOCITY: i32 = 1;
-    pub fn create(context: &WebGl2RenderingContext, program: Program) -> AdvectPipeline {
-        context.use_program(Some(program.program()));
-        context.uniform1i(
-            program
-                .uniforms()
-                .get("u_target")
-                .expect("missing argument")
-                .into(),
-            Self::TARGET,
-        );
-        context.uniform1i(
-            program
-                .uniforms()
-                .get("u_velocity")
-                .expect("missing argument")
-                .into(),
-            Self::VELOCITY,
-        );
-        AdvectPipeline {
-            program,
-            timestep: 0.0,
-            target_texel_size: (0.0, 0.0),
-            velocity_texel_size: (0.0, 0.0),
-        }
-    }
-    pub fn set_arguments(
-        &mut self,
-        context: &WebGl2RenderingContext,
-        timestep: f32,
-        velocity_texture: &BufferedTexture,
-        target_texture: &BufferedTexture,
-    ) -> () {
-        context.use_program(Some(self.program.program()));
-        velocity_texture.attach(Self::VELOCITY);
-        target_texture.attach(Self::TARGET);
+render_pipeline!(BoundaryPipeline, "shaders/boundary.frag");
 
-        if self.timestep != timestep {
-            context.uniform1f(
-                self.program
-                    .uniforms()
-                    .get("u_timestep")
-                    .expect("missing argument")
-                    .into(),
-                timestep,
-            );
-            self.timestep = timestep;
-        }
-
-        if self.velocity_texel_size != velocity_texture.texel_size() {
-            context.uniform2f(
-                self.program
-                    .uniforms()
-                    .get("u_velocity_texel_size")
-                    .expect("missing argument")
-                    .into(),
-                velocity_texture.texel_size().0,
-                velocity_texture.texel_size().1,
-            );
-            self.velocity_texel_size = velocity_texture.texel_size();
-        }
-
-        if self.target_texel_size != target_texture.texel_size() {
-            context.uniform2f(
-                self.program
-                    .uniforms()
-                    .get("u_target_texel_size")
-                    .expect("missing argument")
-                    .into(),
-                target_texture.texel_size().0,
-                target_texture.texel_size().1,
-            );
-            self.target_texel_size = target_texture.texel_size();
-        }
-    }
-}
-
-struct ImpulsePipeline {
-    program: Program,
-    radius: f32,
-    scale: f32,
-    location: (f32, f32),
-    direction: (f32, f32),
-}
-
-impl ImpulsePipeline {
-    const VELOCITY: i32 = 0;
-    pub fn create(context: &WebGl2RenderingContext, program: Program) -> Self {
-        context.use_program(Some(program.program()));
-        context.uniform1i(program.uniforms().get("u_velocity"), Self::VELOCITY);
-        ImpulsePipeline {
-            program,
-            radius: 0.0,
-            scale: 0.0,
-            location: (0.0, 0.0),
-            direction: (0.0, 0.0),
-        }
-    }
-    pub fn set_arguments(
-        &mut self,
-        context: &WebGl2RenderingContext,
-        location: (f32, f32),
-        direction: (f32, f32),
-        radius: f32,
-        scale: f32,
-        target_texture: &BufferedTexture,
-    ) -> () {
-        context.use_program(Some(self.program.program()));
-        target_texture.attach(Self::VELOCITY);
-        if self.location != location {
-            context.uniform2f(
-                self.program.uniforms().get("u_location").unwrap().into(),
-                location.0,
-                location.1,
-            );
-            self.location = location;
-        }
-
-        if self.direction != direction {
-            context.uniform2f(
-                self.program.uniforms().get("u_direction").unwrap().into(),
-                direction.0,
-                direction.1,
-            );
-            self.direction = direction;
-        }
-
-        if self.radius != radius {
-            context.uniform1f(
-                self.program.uniforms().get("u_radius").unwrap().into(),
-                radius,
-            );
-            self.radius = radius;
-        }
-
-        if self.scale != scale {
-            context.uniform1f(
-                self.program.uniforms().get("u_scale").unwrap().into(),
-                scale,
-            );
-            self.scale = scale;
-        }
-    }
-}
-
-struct DivergencePipeline {
-    program: Program,
-    texel_size: (f32, f32),
-}
-
-impl DivergencePipeline {
-    const VELOCITY: i32 = 0;
-    pub fn create(context: &WebGl2RenderingContext, program: Program) -> Self {
-        context.use_program(Some(program.program()));
-        context.uniform1i(
-            program.uniforms().get("u_velocity").unwrap().into(),
-            Self::VELOCITY,
-        );
-        DivergencePipeline {
-            program,
-            texel_size: (0.0, 0.0),
-        }
-    }
-    pub fn set_arguments(
-        &mut self,
-        context: &WebGl2RenderingContext,
-        velocity_texture: &BufferedTexture,
-    ) {
-        context.use_program(Some(self.program.program()));
-        velocity_texture.attach(Self::VELOCITY);
-        if self.texel_size != velocity_texture.texel_size() {
-            context.uniform2f(
-                self.program.uniforms().get("u_texel_size").unwrap().into(),
-                velocity_texture.texel_size().0,
-                velocity_texture.texel_size().1,
-            );
-            self.texel_size = velocity_texture.texel_size();
-        }
-    }
-}
-
-struct GradientSubtractPipeline {
-    program: Program,
-    texel_size: (f32, f32),
-}
-
-impl GradientSubtractPipeline {
-    const VELOCITY: i32 = 0;
-    const PRESSURE: i32 = 1;
-    pub fn create(context: &WebGl2RenderingContext, program: Program) -> Self {
-        context.use_program(Some(program.program()));
-        context.uniform1i(
-            program.uniforms().get("u_velocity").unwrap().into(),
-            Self::VELOCITY,
-        );
-        context.uniform1i(
-            program.uniforms().get("u_pressure").unwrap().into(),
-            Self::PRESSURE,
-        );
-        GradientSubtractPipeline {
-            program,
-            texel_size: (0.0, 0.0),
-        }
-    }
-    pub fn set_arguments(
-        &mut self,
-        context: &WebGl2RenderingContext,
-        velocity_texture: &BufferedTexture,
-        pressure_texture: &BufferedTexture,
-    ) -> () {
-        debug_assert!(velocity_texture.texel_size() == pressure_texture.texel_size());
-        context.use_program(Some(self.program.program()));
-        velocity_texture.attach(Self::VELOCITY);
-        pressure_texture.attach(Self::PRESSURE);
-        if self.texel_size != velocity_texture.texel_size() {
-            context.uniform2f(
-                self.program.uniforms().get("u_texel_size").unwrap().into(),
-                velocity_texture.texel_size().0,
-                velocity_texture.texel_size().1,
-            );
-            self.texel_size = velocity_texture.texel_size();
-        }
-    }
-}
-
-struct JacobiPipeline {
-    program: Program,
-    alpha: (f32, f32),
-    r_beta: (f32, f32),
-    texel_size: (f32, f32),
-}
-
-impl JacobiPipeline {
-    const INITIAL: i32 = 0;
-    const SOLUTION: i32 = 1;
-    pub fn create(context: &WebGl2RenderingContext, program: Program) -> Self {
-        context.use_program(Some(program.program()));
-        context.uniform1i(
-            program.uniforms().get("u_initial").unwrap().into(),
-            Self::INITIAL,
-        );
-        context.uniform1i(
-            program.uniforms().get("u_solution").unwrap().into(),
-            Self::SOLUTION,
-        );
-        JacobiPipeline {
-            program,
-            alpha: (0.0, 0.0),
-            r_beta: (0.0, 0.0),
-            texel_size: (0.0, 0.0),
-        }
-    }
-    pub fn set_arguments(
-        &mut self,
-        context: &WebGl2RenderingContext,
-        alpha: (f32, f32),
-        r_beta: (f32, f32),
-        initial_texture: &BufferedTexture,
-        solution_texutre: &BufferedTexture,
-    ) -> () {
-        debug_assert!(initial_texture.texel_size() == solution_texutre.texel_size());
-        initial_texture.attach(Self::INITIAL);
-        solution_texutre.attach(Self::SOLUTION);
-        context.use_program(Some(self.program.program()));
-        if self.alpha != alpha {
-            context.uniform2f(
-                self.program.uniforms().get("u_alpha").unwrap().into(),
-                alpha.0,
-                alpha.1,
-            );
-            self.alpha = alpha;
-        }
-
-        if self.r_beta != r_beta {
-            context.uniform2f(
-                self.program.uniforms().get("u_r_beta").unwrap().into(),
-                r_beta.0,
-                r_beta.1,
-            );
-            self.r_beta = r_beta;
-        }
-
-        if self.texel_size != initial_texture.texel_size() {
-            context.uniform2f(
-                self.program.uniforms().get("u_texel_size").unwrap().into(),
-                initial_texture.texel_size().0,
-                initial_texture.texel_size().1,
-            );
-            self.texel_size = initial_texture.texel_size();
-        }
-    }
-}
+render_pipeline!(GradientSubtractPipeline, "shaders/gradient.frag");
 
 fn canvas_fill(context: WebGl2RenderingContext, mouse: Rc<UseMouseReturn>) {
     context.get_extension("EXT_color_buffer_float").unwrap();
@@ -580,16 +217,24 @@ fn canvas_fill(context: WebGl2RenderingContext, mouse: Rc<UseMouseReturn>) {
         let now = window().performance().unwrap().now();
 
         // Velocity Boundary
-        boundary_pipeline.set_arguments(&context, -1.0, &boundary_texture, velocity_texture.read());
+        boundary_pipeline.set_arguments(
+            &context,
+            velocity_texture.read(),
+            &boundary_texture,
+            velocity_texture.read().texel_size(),
+            -1.0,
+        );
         quad.blit(Some(velocity_texture.write()));
         velocity_texture.swap();
 
         // Advect Velocity
         advect_pipeline.set_arguments(
             &context,
+            velocity_texture.read(),
+            velocity_texture.read(),
+            velocity_texture.read().texel_size(),
+            velocity_texture.read().texel_size(),
             timestep,
-            velocity_texture.read(),
-            velocity_texture.read(),
         );
         quad.blit(Some(velocity_texture.write()));
         velocity_texture.swap();
@@ -597,9 +242,11 @@ fn canvas_fill(context: WebGl2RenderingContext, mouse: Rc<UseMouseReturn>) {
         // Advect Dye
         advect_pipeline.set_arguments(
             &context,
-            timestep,
-            velocity_texture.read(),
             dye_texture.read(),
+            velocity_texture.read(),
+            dye_texture.read().texel_size(),
+            velocity_texture.read().texel_size(),
+            timestep,
         );
         quad.blit(Some(dye_texture.write()));
         dye_texture.swap();
@@ -613,11 +260,11 @@ fn canvas_fill(context: WebGl2RenderingContext, mouse: Rc<UseMouseReturn>) {
             if prev_input_time.is_some() && prev_frame.is_some() && prev_input_time >= prev_frame {
                 impulse_pipeline.set_arguments(
                     &context,
+                    velocity_texture.read(),
                     cur_mouse,
                     (cur_mouse.0 - prev_mouse.0, cur_mouse.1 - prev_mouse.1),
-                    force_radius,
                     force_scale,
-                    velocity_texture.read(),
+                    force_radius,
                 );
                 quad.blit(Some(velocity_texture.write()));
                 velocity_texture.swap();
@@ -636,17 +283,22 @@ fn canvas_fill(context: WebGl2RenderingContext, mouse: Rc<UseMouseReturn>) {
         for _ in 0..30 {
             jacobi_pipeline.set_arguments(
                 &context,
+                velocity_texture.read(),
+                &temp_texture,
+                velocity_texture.read().texel_size(),
                 diffusion_alpha,
                 diffusion_beta,
-                &temp_texture,
-                velocity_texture.read(),
             );
             quad.blit(Some(velocity_texture.write()));
             velocity_texture.swap();
         }
 
         // Compute Divergance
-        divergence_pipeline.set_arguments(&context, velocity_texture.read());
+        divergence_pipeline.set_arguments(
+            &context,
+            velocity_texture.read(),
+            velocity_texture.read().texel_size(),
+        );
         quad.blit(Some(&temp_texture));
 
         // Compute Pressure
@@ -658,31 +310,44 @@ fn canvas_fill(context: WebGl2RenderingContext, mouse: Rc<UseMouseReturn>) {
         for _ in 0..40 {
             boundary_pipeline.set_arguments(
                 &context,
-                1.0,
-                &boundary_texture,
                 pressure_texture.read(),
+                &boundary_texture,
+                pressure_texture.read().texel_size(),
+                1.0,
             );
             quad.blit(Some(pressure_texture.write()));
             pressure_texture.swap();
 
             jacobi_pipeline.set_arguments(
                 &context,
+                pressure_texture.read(),
+                &temp_texture,
+                pressure_texture.read().texel_size(),
                 pressure_alpha,
                 pressure_beta,
-                &temp_texture,
-                pressure_texture.read(),
             );
             quad.blit(Some(pressure_texture.write()));
             pressure_texture.swap();
         }
 
         // Reapply Boundaries
-        boundary_pipeline.set_arguments(&context, -1.0, &boundary_texture, velocity_texture.read());
+        boundary_pipeline.set_arguments(
+            &context,
+            velocity_texture.read(),
+            &boundary_texture,
+            velocity_texture.read().texel_size(),
+            -1.0,
+        );
         quad.blit(Some(velocity_texture.write()));
         velocity_texture.swap();
 
         // Gradient Subtraction
-        gradient_pipeline.set_arguments(&context, velocity_texture.read(), pressure_texture.read());
+        gradient_pipeline.set_arguments(
+            &context,
+            velocity_texture.read(),
+            pressure_texture.read(),
+            velocity_texture.read().texel_size(),
+        );
         quad.blit(Some(velocity_texture.write()));
         velocity_texture.swap();
 
